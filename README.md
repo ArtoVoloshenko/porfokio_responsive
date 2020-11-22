eb5dc4c70ee6ed528ff48999b509b3e05c6e25cf
ArtoVoloshenko <amandarinsochi@list.ru>
2020-11-22 15:16:21 +0300

    add all files to my portfolio

---
+++ README.md
@@ -0,0 +1,7 @@
+Responsive Portfolio
+============
+[![Live Demo](https://img.shields.io/badge/demo-online-balck.svg)](http://arto-web.site/) 
+
+
+Although the design is similar to that tutorial, my code is heavily modified as I want to recreate the website using my own skill and experience 👨‍🔧
+
---
+++ scripts/animationText.js
@@ -0,0 +1,20 @@
+new TypeIt("#simpleUsage", {
+    strings: "",
+    speed: 100,
+    waitUntilVisible: true
+}).go();
+
+new TypeIt("#simpleUsage2", {
+    strings: "Главная",
+    speed: 100,
+    waitUntilVisible: true
+}).go();
+new TypeIt("#simpleUsage3", {
+    strings: "",
+    speed: 50,
+    waitUntilVisible: true
+}).go();new TypeIt("#simpleUsage4", {
+    strings: "",
+    speed: 100,
+    waitUntilVisible: true
+}).go();
